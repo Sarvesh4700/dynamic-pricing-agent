c694cc4a227a34e89bbd13b7ad1a392df69b055a
Sarvesh4700 <sarvesh47007@gmail.com>
2026-08-25 09:55:40 +0530

    Remove optional LLM agent layer

--- conftest.py
+++
@@ -1,7 +0,0 @@
-"""Makes 'src' and 'tests' importable when pytest is invoked from anywhere."""
-import os
-import sys
-
-ROOT = os.path.dirname(os.path.abspath(__file__))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
